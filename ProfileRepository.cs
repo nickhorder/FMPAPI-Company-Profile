4dfe77183332da7f7fe99c40a20e00475e47524c
nickhorder <thunderstruck1986@hotmail.co.uk>
2021-10-24 18:30:47 +0100

    Add project files.

---
+++ ProfileRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+//namespace FMPAPICompanyProfile
+//{
+public class ProfileRepository
+    { 
+/*
+     *  Definitions of Json Properties in the FMP API, converted to their equivalents in 
+     *  this C# program. This is essentially a mapper
+*/
+    // Values from Company Key Stats (profile as endpoint)
+
+            [JsonPropertyName("companyName")]
+            public string CompanyName { get; set; }
+
+            [JsonPropertyName("exchangeShortName")]
+            public string ExchangeShortName { get; set; }
+
+            [JsonPropertyName("description")]
+            public string Description { get; set; }
+        }
+    
---
+++ TickerStore.cs
@@ -0,0 +1,38 @@
+﻿public class TickerStore
+{
+    string[] items;
+    public TickerStore()
+    {
+        items = new string[] {
+            "GSK.L",
+            "AZN.L",
+            "NXT.L",
+            "EXPN.L",
+            "IDEA.L" };
+    }
+    public TickerEnumerator GetEnumerator()
+    {
+        return new TickerEnumerator(this);
+    }
+
+// Declare the enumerator class:  
+    public class TickerEnumerator
+    {
+        int nIndex;
+        TickerStore collection;
+        public TickerEnumerator(TickerStore coll)
+        {
+            collection = coll;
+            nIndex = -1;
+        }
+        public bool MoveNext()
+        {
+            nIndex++;
+            return (nIndex < collection.items.Length);
+        }
+
+        public string Current => collection.items[nIndex];
+    }
+}
+
+  
